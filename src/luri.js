16cf1ce0ba316bb22915199f66b93ea1fe098e15
Manix <admin@manix.info>
2017-07-09 13:16:31 +0300

    Update luri.js

--- src/luri.js
+++ src/luri.js
@@ -68,11 +68,12 @@
   ["a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "keygen", "label", "legend", "li", "link", "main", "map", "mark", "math", "menu", "menuitem", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rb", "rp", "rt", "rtc", "ruby", "s", "samp", "script", "section", "select", "slot", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr"].forEach(function (tag) {
     luri[tag.toUpperCase()] = function (props) {
       if (typeof props === "string") {
-        return luri.construct({node: tag, html: props});
+        props = {node: tag, html: props};
       } else {
         props.node = tag;
-        return luri.construct(props);
       }
+      
+      return luri.construct(props);
     };
   });
 
